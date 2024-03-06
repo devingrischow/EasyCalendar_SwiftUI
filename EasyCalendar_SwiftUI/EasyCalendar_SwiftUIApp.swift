4928b1da33977e3ab1539a45868ea6fe54c7e67a
devingrischow <devingrischow@outlook.com>
2024-03-05 19:41:03 -0600

    Initial Commit

---
+++ EasyCalendar_SwiftUI/EasyCalendar_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyCalendar_SwiftUIApp.swift
+//  EasyCalendar_SwiftUI
+//
+//  Created by Devin Grischow on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct EasyCalendar_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
